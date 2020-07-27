5bc3d6a72535d1fc9654dd0780a0049bb840408d
Steven K <kleist.steven+github@gmail.com>
2020-07-27 16:04:02 +0200

    Create init.lua

---
+++ web/session/init.lua
@@ -0,0 +1,15 @@
+local session = {
+  _VERSION = "",
+  _DESCRIPTION = "",
+  _LICENSE = [[]],
+}
+
+
+function session.new()
+  return {}
+end
+
+-------------------------------------------------------------------------------
+-- Returning module
+-------------------------------------------------------------------------------
+return session
