79f2a901fc525c67b12b9b39a7895e9a8947995f
Steven K <kleist.steven+github@gmail.com>
2020-07-27 13:47:48 +0200

    Update init.lua

--- web/response/init.lua
+++ web/response/init.lua
@@ -4,10 +4,40 @@ local response = {
   _LICENSE = [[]],
 }
 
+require "strong"
+local json = require "json"
+local etlua = require "etlua"
+
+
+local http_status = {
+  [200] = "OK",
+}
+
+
+local response_ = {
+  buffer = {
+    status = "",
+    head = {},
+    body = {},
+  },
+  renderer = etlua,
+  views = os.getenv("APP_VIEWS"),
+}
+
 -------------------------------------------------------------------------------
 -- member functions
 -------------------------------------------------------------------------------
-function response:send(content) end
+function response:send()
+  local data = {}
+  
+  data = table.concat({
+    response_.buffer.status,
+    (table.concat(response_.buffer.head, "\r\n")),
+    "",
+    (table.concat(response_.buffer.body, "\r\n"))
+  }, "\r\n")
+  io.write(data)
+end
 
 
 function response:status(status)
@@ -16,10 +46,50 @@ function response:status(status)
     return error("[response:status] {status} not convertable to number!")
   end
   
+  response_.buffer.status = "HTTP/1.1 %d %s" % {status, http_status[status]}
+  
+  return self
+  
 end
 
 
-function response:json(content) end
+function response:render(view, data)
+  local tpl_data = io.open(response_.views .. [[\%s.html]] % view):read("*a")
+  local tpl = etlua.compile(tpl_data)
+  
+  table.insert(response_.buffer.body, tpl(data))
+  return self
+end
+
+
+function response:json(content)
+  local data = ""
+  if type(content) == "table" then
+    data = json.encode(content)
+  else
+    data = content
+  end
+  table.insert(response_.buffer.body, data)
+  
+  return self
+end
+
+
+function response:cookie(name, value, options)
+  local cookie = "Set-Cookie: %s=%s" % {name, value}
+  local opts   = ""
+  if options ~= nil then
+    opts = table.concat(options, "; ")
+  end
+  table.insert(response_.buffer.head, cookie .. (opts ~= "" and "; " .. opts or ""))
+  return self
+end
+
+
+function response:header(name, value)
+  table.insert(response_.buffer.head, "%s: %s" % {name, value})
+  return self
+end
 
 
 
